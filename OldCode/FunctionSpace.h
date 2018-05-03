70ad5e332141eadb2bc3edbe000f3290936fb66e
muzzylexy <33651474+muzzylexy@users.noreply.github.com>
2018-05-03 10:02:04 +0800

    the 1st version of FunctionSpace

---
+++ OldCode/FunctionSpace.h
@@ -0,0 +1,50 @@
+#include "Point.hpp"
+
+class Interpolation;
+
+template<class NumericalMethodUtility, class Dimension>
+class FunctionSpace{
+
+public:
+
+	template<class Dimension>
+	using GeometryDataPtr = std::shared_ptr<GeometryData<Dimension>>;
+
+	using InterpolationPtr = std::shared_ptr<Interpolation>;
+
+	FunctionSpace() = delete;
+
+	FunctionSpace(const GeometryDataPtr& geo_data_, const InterpolationPtr& method_) :
+		geo_data(geo_data_), method(method_)
+	{
+		initialize();
+	}
+
+	FunctionSpace(const GeometryDataPtr& geo_data_, const InterpolationPtr& method_, const Reorder& reorder) :
+		geo_data(geo_data_), method(method_)
+	{
+		initialize();
+		reorder(solution_points);
+	}
+
+	double evaluate_shape(const PointList& position){
+		return method_.evaluate_shape(position);
+	}
+
+	bool evaluate(Dof& dof, const PointList& position){
+		method_.evaluate(dof, position);
+		return true;
+	}
+
+private:
+
+	bool initialize(){
+		// new pointlist here
+		return true;
+	}
+
+	GeometryDataPtr geo_data;
+	InterpolationPtr method;
+	std::shared_ptr<PointList<Dimension>> solution_points;
+
+};
