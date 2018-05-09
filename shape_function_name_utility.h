128b9bcb6f2914542d472eb5664bf099d8a3792f
muzzylexy <33651474+muzzylexy@users.noreply.github.com>
2018-05-09 11:07:35 +0800

    May. 09. 2018 First version of ShapeFunction

---
+++ shape_function_name_utility.h
@@ -0,0 +1,21 @@
+
+#ifndef ARTCFD_SHAPE_FUNCTION_NAME_UTILITY_HPP
+#define ARTCFD_SHAPE_FUNCTION_NAME_UTILITY_HPP
+
+// Std Lib Include Zone
+
+// ArtPDE Lib Include Zone
+
+
+namespace art_pde{
+
+	struct FunctionName{};
+
+	struct LagrangePoly :public FunctionName{};
+
+	struct Multiquadric :public FunctionName{};
+}
+
+
+#endif //ARTCFD_SHAPE_FUNCTION_NAME_UTILITY_HPP
+
