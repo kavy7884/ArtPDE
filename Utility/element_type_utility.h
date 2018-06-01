ba16a81ef0b91823e203acab534bdbf961bfa659
muzzylexy <33651474+muzzylexy@users.noreply.github.com>
2018-06-01 12:27:45 +0800

    June. 1. 2018

--- Utility/element_type_utility.h
+++ Utility/element_type_utility.h
@@ -29,7 +29,17 @@ namespace art_pde{
 
 	struct Tetra4 :public ElementType{ static const int NUM = 4; };
 
-	struct Prism5 :public ElementType{ static const int NUM = 5; };
+	struct Prism6 :public ElementType{ static const int NUM = 6; };
+
+	struct Pyramid5 :public ElementType{ static const int NUM = 5; };
+
+	struct Tetra10 :public ElementType{ static const int NUM = 10; };
+
+	struct Prism15 :public ElementType{ static const int NUM = 15; };
+
+	struct Hexa20 :public ElementType{ static const int NUM = 20; };
+
+	struct Pyramid13 :public ElementType{ static const int NUM = 13; };
 
 	struct ScatterPoint{};
 
